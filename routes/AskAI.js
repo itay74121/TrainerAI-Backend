2ad1382e2ad2f3e8b76aeee43863b24cda8ac82d
Itay <itay74121@gmail.com>
2024-12-21 15:58:58 +0200

    tried connecting with AI

--- routes/AskAI.js
+++ routes/AskAI.js
@@ -1,9 +1,27 @@
+require("dotenv").config()
 const express = require('express');
 const router = express.Router();
+import OpenAI from "openai";
+//const OpenAI = require("openai")
 
+const openai= new OpenAI(process.env.OPENAI_API_KEY)
 
-router.get('/Question', (req, res) => {
-    res.send('AskAI Question');
+router.get('/Question', async (req, res) => {
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": "Write a haiku about programming."
+              }
+            ]
+          }
+        ]
+      });
+    res.send(response);
 });
 
 exports.AskAIrouter = router;
